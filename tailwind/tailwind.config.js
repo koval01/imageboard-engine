57d904d7bb5cb4bdff688416fd5e0b24affe554d
Koval Yaroslav <ksvarog2004@gmail.com>
2026-02-06 19:23:43 +0100

    Update

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,13 +1,28 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: ["../templates/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Kanit', ...fontFamily.sans]
+        // 2ch uses standard system fonts for readability
+        sans: ['Arial', 'Helvetica', 'sans-serif', ...fontFamily.sans],
+      },
+      colors: {
+        // The classic Dvach/Makaba palette
+        'dvach-bg': '#eeeeee',        // Main background
+        'dvach-post-bg': '#dddddd',   // Reply background
+        'dvach-orange': '#ff9933',    // Highlights/Logos
+        'dvach-text': '#333333',      // Main text
+        'dvach-red': '#aa0000',       // Sage/Admin
+        'dvach-green': '#789922',     // >Greentext
+        'dvach-link': '#ff6600',      // Links
+        'dvach-dark': '#222222',      // Navbar bg
+        'dvach-border': '#b7c5d9',    // Post borders
+      },
+      fontSize: {
+        'tiny': '0.7rem',
       }
     },
   },
@@ -15,7 +30,7 @@ module.exports = {
     require("daisyui")
   ],
   daisyui: {
-    themes: ["dark"]
+    // We largely want to override DaisyUI defaults for that "old school" look
+    themes: ["light"]
   }
 }
-
